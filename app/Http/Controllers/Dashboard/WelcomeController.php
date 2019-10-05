2856b99470e188979af589ae4197fb2054cad798
Abdelrahman Ghanem <a.ghanem2244@gmail.com>
2019-10-05 16:58:17 +0200

    add features to welcome section

--- app/Http/Controllers/Dashboard/WelcomeController.php
+++ app/Http/Controllers/Dashboard/WelcomeController.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Controllers\Dashboard;
 
+use App\Product;
+use App\User;
+use App\Order;
+use App\Category;
+use App\Client;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class WelcomeController extends Controller
 {
     public function index() {
-    	return view('dashboard.welcome');
+    	$products_count = Product::count(); 
+    	$users_count = User::whereRoleIs('admin')->count(); 
+    	$clients_count = Client::count(); 
+    	$categories_count = Category::count(); 
+ 
+    	$sales_data = Order::select(
+    		DB::raw('YEAR(created_at) as year'),
+    		DB::raw('MONTH(created_at) as month'),
+    		DB::raw('SUM(total_price) as sum'),
+    	)->groupBy('month')->get();
+
+
+    	return view('dashboard.welcome', compact('products_count','users_count','clients_count','categories_count', 'sales_data'));
     } // end of index
 } // end of controller
